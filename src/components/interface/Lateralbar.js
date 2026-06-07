2d96e67bfdede6cfa4a4baa0f85cfdfba6495971
Pelayogm <pelagrmenendez12@gmail.com>
2026-06-08 00:57:55 +0200

    v0.9.1 Navbar y CSS adaptado al nuevo componente

--- src/components/interface/Lateralbar.js
+++ src/components/interface/Lateralbar.js
@@ -3,13 +3,17 @@ import carIcon from '../../img/ui/car-ui.svg';
 import nearBy from '../../img/ui/nearby-ui.svg';
 import priceIcon from '../../img/ui/price-ui.svg';
 import settingsIcon from '../../img/ui/settings-ui.svg'
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import MyVehiclesMenu from './LM-FILES/LM-MyVehicles-menu';
 import SettingsMenu from './LM-FILES/LM-Settings-menu';
+import addCarSelectBrand from './LM-SUBFILES/LM-MY-VEHICLES/LM-AddCar-SelectBrand'
+import {lateralbarAddCarContext} from './LM-SUBFILES/LM-MY-VEHICLES/LM-AddCar-Context';
+import AddCarSelectBrand from './LM-SUBFILES/LM-MY-VEHICLES/LM-AddCar-SelectBrand';
 
 export default function Lateralbar({ visible }) {
     const [openTab, setOpenTab] = useState(null)
     const [activeMenu, setActiveMenu] = useState('cartab')
+    const {addCarMenu, setAddCarMenu, addCarTab} = useContext(lateralbarAddCarContext)
 
     const sectionsMenu = {
         cartab: <MyVehiclesMenu/>,
@@ -20,7 +24,7 @@ export default function Lateralbar({ visible }) {
 
     return (
         <>
-            <div className={`lateralbar ${visible ? 'visible' : ''}`}>
+            <div className={`lateralbar${visible ? ' visible' : ''}${addCarMenu ? ' addCarMenu': ''}`}>
                 <div className='lateralbar-menu'>
                     <ul>
                         <button onClick={() => setActiveMenu('cartab')}><img src={carIcon} alt='car-menu' className='lateral-menu-icons'/></button>
@@ -29,9 +33,16 @@ export default function Lateralbar({ visible }) {
                         <button onClick={() => setActiveMenu('settings')}><img src={settingsIcon} alt='settings-menu' className='lateral-menu-icons'/></button>
                     </ul>
                 </div>
-                <div className="lateralbar-variable">
-                    {sectionsMenu[activeMenu]}
-                </div>
+                { !addCarMenu && 
+                    <div className="lateralbar-variable">
+                        {sectionsMenu[activeMenu]}
+                    </div>
+                }
+                {addCarMenu &&
+                    <div className={`lateralbar-variable-extended${addCarTab ? ' carTabOpen' : ''}`}>
+                        <AddCarSelectBrand/>
+                    </div>
+                }
             </div>
         </>
     )
